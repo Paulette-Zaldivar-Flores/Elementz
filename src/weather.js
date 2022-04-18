6ac923971ed1e9ed2fd40637aded3dbcb44f394d
Void321 <96970580+Void321@users.noreply.github.com>
2022-04-18 21:40:14 +0900

    imported react-weather app from codesandbox
    -cleaned up react files
    -moved imgaes folder into src

--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,66 @@
-import React from "react";
-import axios from "axios";
+import "./Weather.css";
 
 export default function Weather() {
-  return <h2> Weather App</h2>;
+  let weatherData = {
+    city: "New York",
+    temperature: 19,
+    date: "Tuesday 10:00",
+    description: "Cloudy",
+    imgUrl: "/src/images/weather.svg",
+
+    humidity: 80,
+    wind: 10,
+  };
+
+  return (
+    <div className="Weather">
+      <form className="mb-4">
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Search for a city..."
+              className="form-control"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-2">
+            <input type="submit" value="Search" className="btn" />
+          </div>
+        </div>
+      </form>
+
+      <div className="overview">
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Last updated: {weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-5">
+          <div className="clearfix weather-temperature">
+            <img
+              src={weatherData.imgUrl}
+              alt={weatherData.description}
+              className="float-left"
+            />
+
+            <div className="float-left">
+              <strong>{weatherData.temperature}</strong>
+              <span className="units">
+                <a href="/">°C</a>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind} km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
