d957cf57c6778975a3a623e93adfa1ab5335d723
Void321 <96970580+Void321@users.noreply.github.com>
2022-04-21 23:22:47 +0900

    updated weather.js
    -useState
    -added handle response with setWeatherDate information
    -added api information and integrated API call using axios
    -used interpolation for all of api call weather info

--- src/weather.js
+++ src/weather.js
@@ -1,68 +1,81 @@
+import React, { useState } from "react";
 import "./Weather.css";
 import img from "./images/weather.svg";
 import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
 
 export default function Weather() {
-  let weatherData = {
-    city: "New York",
-    temperature: 19,
-    date: "Tuesday 10:00",
-    description: "Cloudy",
-    imgUrl: img,
-
-    humidity: 80,
-    wind: 10,
-  };
-
-  return (
-    <div className="Weather">
-      <form className="mb-4">
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Search for a city..."
-              className="form-control"
-              autoComplete="off"
-            />
-          </div>
-          <div className="col-2">
-            <input type="submit" value="Search" className="btn" />
-          </div>
-        </div>
-      </form>
-
-      <div className="overview">
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>Last updated: {weatherData.date}</li>
-          <li>{weatherData.description}</li>
-        </ul>
-      </div>
-      <div className="row">
-        <div className="col-5">
-          <div className="clearfix weather-temperature">
-            <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
-              className="float-left"
-            />
-
-            <div className="float-left">
-              <strong>{weatherData.temperature}</strong>
-              <span className="units">
-                <a href="/">°C</a>
-              </span>
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  function handleResponse(response) {
+    setWeatherData({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      imgUrl: img,
+    });
+    setReady(true);
+  }
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form className="mb-4">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Search for a city..."
+                className="form-control"
+                autoComplete="off"
+              />
+            </div>
+            <div className="col-2">
+              <input type="submit" value="Search" className="btn" />
             </div>
           </div>
-        </div>
-        <div className="col-6">
+        </form>
+
+        <div className="overview">
+          <h1>{weatherData.city}</h1>
           <ul>
-            <li>Humidity: {weatherData.humidity}%</li>
-            <li>Wind: {weatherData.wind} km/h</li>
+            <li>Last updated: {weatherData.date}</li>
+            <li className="text-capitalize">{weatherData.description}</li>
           </ul>
         </div>
+        <div className="row">
+          <div className="col-5">
+            <div className="clearfix weather-temperature">
+              <img
+                src={weatherData.imgUrl}
+                alt={weatherData.description}
+                className="float-left"
+              />
+
+              <div className="float-left">
+                <strong>{Math.round(weatherData.temperature)}</strong>
+                <span className="units">
+                  <a href="/">°C</a>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "b81e7138d4f18ecdce4149c89f6f0058";
+    let city = "Tijuana";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
