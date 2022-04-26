b8e08bcc33eb92e94016cfca354ef7abda3d9db5
Void321 <96970580+Void321@users.noreply.github.com>
2022-04-26 20:58:12 +0900

    Update WeatherTemperature.js
    -updated with useState and function showFahreinheit and showCelsius
    -made C and F links

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function WeatherTemperature(props) {
-  return (
-    <div className="WeatherTemp">
-      <strong>{Math.round(props.celsius)}</strong>
-      <span className="units">
-        <a href="/">°C</a>
-      </span>
-    </div>
-  );
+export default function Temperature(props) {
+  const [unit, setUnit] = useState("celsius");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  if (unit === "celsius") {
+    return (
+      <div className="Temperature">
+        <span className="temp">{Math.round(props.celsius)}</span>{" "}
+        <span className="units">
+          °C |{" "}
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
+    return (
+      <div className="Temperature">
+        <span className="temp">{Math.round(fahrenheit)}</span>{" "}
+        <span className="units">
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>
+          °F
+        </span>
+      </div>
+    );
+  }
 }
