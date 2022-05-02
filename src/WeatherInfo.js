80e7b1bd688d5c36e610f3fd69e918c73e0590ea
Void321 <96970580+Void321@users.noreply.github.com>
2022-05-02 18:01:21 +0900

    weatherforecast.css
    -adjusted canvas size
    -deleted unused div classes

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -20,25 +20,24 @@ export default function WeatherInfo(props) {
         <div className="col">
           <div className="card">
             <div className="weather-temperature">
-              <div className="float-left">
-                <WeatherIcon code={props.data.icon} size={80} />
-              </div>
-              <div className="float-left">
-                <WeatherTemperature celsius={props.data.temperature} />
-              </div>
+              <WeatherIcon code={props.data.icon} size={80} />
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
         <div className="col">
           <div className="card">
-            <ul>
-              <li>
-                {" "}
-                <img className="temp-pic" src={img} alt="" />{" "}
-              </li>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: {props.data.wind} km/h</li>
-            </ul>
+            <div className="humidity-wind">
+              <ul>
+                <li>
+                  {" "}
+                  <img className="temp-pic" src={img} alt="" />{" "}
+                </li>
+
+                <li>Humidity: {props.data.humidity}%</li>
+                <li>Wind: {props.data.wind} km/h</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
