da0018e64fbfa2e12fafa1e606a933c81460addf
Void321 <96970580+Void321@users.noreply.github.com>
2022-05-03 11:52:18 +0900

    app.css. index.js, src
    -(app.css )chaaged gradient colors
    -(index.js)added video backround index.js
    -added video in src folder
    -(weather.css)- added video css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Weather from "./weather";
 import Footer from "./Footer";
+import bgImage from "./video/background-image.mp4";
 
 import "./App.css";
 
@@ -9,6 +10,9 @@ function App() {
   return (
     <div className="App">
       <div className="container">
+        <video autoPlay loop muted>
+          <source src={bgImage} type="video/mp4" />
+        </video>
         <Weather city="Mexico City" />
         <Footer />
       </div>
