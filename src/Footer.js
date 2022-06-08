93c44db4290594ece541e901019ab80939c0a4fe
Void321 <paulettz@icloud.com>
2022-06-08 21:11:49 +0900

    updating contact links with font awesome icon
    -github icon and linkedin con in footer.js
    -imported fontawesome info footer.js
    -imported fontawesome kit into index.html

--- src/Footer.js
+++ src/Footer.js
@@ -1,22 +1,35 @@
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubAlt, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
     <footer className="link">
+      Open Source Code:{"   "}
       <a
         href="https://github.com/Void321/React-Weather-App"
         target="_blank"
         rel="noreferrer"
       >
-        Open-source code
-      </a>{" "}
-      |by: Paulette Zaldivar Flores | Hosted on{" "}
+        <FontAwesomeIcon icon={faGithubAlt}></FontAwesomeIcon>
+      </a>
+      |by: Me{" "}
+      <a
+        href="https://www.linkedin.com/in/paulettezaldivarflores3"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+      </a>
+      | Hosted on{" "}
       <a
         href="https://stupendous-pithivier-e67924.netlify.app/"
         target="blank"
         rel="noreferrer"
       >
-        Netlify
+        {" "}
+        <span className="Netlify">Netlify</span>
       </a>
     </footer>
   );
